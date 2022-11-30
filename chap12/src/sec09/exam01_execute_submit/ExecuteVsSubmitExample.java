812d8a25beda6b2dfd43510f8046d42b74aa8709
khyunji99 <hyungi246@naver.com>
2022-11-30 21:07:17 +0900

    12장_스레드풀_스레드풀 생성 및 종료만의 내용만 아주 간단하게 다룸

---
+++ chap12/src/sec09/exam01_execute_submit/ExecuteVsSubmitExample.java
@@ -0,0 +1,36 @@
+package sec09.exam01_execute_submit;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class ExecuteVsSubmitExample {
+	public static void main(String[] args) throws Exception {
+		// Thread Pool  생성
+		ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+		for (int i = 0; i < 10; i++) {
+			// Runnable 인터페이스에 익명구현객체 대입
+			Runnable runnable = new Runnable() {
+				@Override
+				public void run() {
+					// 스레드 총 개수 및 작업 스레드 이름 출력
+					ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) executorService;
+					int poolSize = threadPoolExecutor.getPoolSize();
+					String threadName = Thread.currentThread().getName();
+					System.out.println("[총 스레드 개수: " + poolSize + "] 작업 스레드 이름: " + threadName);
+					// 예외 발생 시킴
+					int value = Integer.parseInt("삼");
+				}
+			};
+			
+			// Thread Pool 실행
+			executorService.execute(runnable);
+			// executorService.submit(runnable);
+
+			Thread.sleep(10);
+		}
+
+		executorService.shutdown();
+	}
+}
